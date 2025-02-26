c69dce4186a3a259dbe812bf6ad6914bd32ffb29
Mia Seo <seoy804@newschool.edu>
2025-02-26 06:38:04 -0500

    Read me add and clean coding and attribution add

---
+++ README.md
@@ -0,0 +1,8 @@
+Project4_ Links _ Mia Seo
+-I used the Are.na API to link data to my website and also learned basic JavaScript.
+
+A Digital Archive of "Stay Creative"
+
+This website is a curated collection by Jenny yen.
+
+Creativity is an important part of being a designer. This collection contains methods for maintaining creativity, creative ideas that inspire me, and resources (websites, books) that discuss creativity.
--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,6 @@
 // Wish to make Rotated cube with random linked media from arena,
+// rotated cube with random image from API is the most hardest part
+// I found this tools: CHAT GPT an https://labex.io/tutorials/css-3d-rotating-cube-165641,d
 //Function to update each face of the cube
 function updateCubeFaces() {
     const cubeFaces = ["front", "back", "right", "left", "top", "bottom"];
@@ -17,6 +19,7 @@ function updateCubeFaces() {
 
 setInterval(updateCubeFaces, updateInterval);
 
+// I want to make pop-up modeal for channel-description
 //https://developer.mozilla.org/en-US/docs/Web/CSS/::backdrop
 // modal open and close- not easy to understand
 
@@ -47,6 +50,9 @@ let showAllButton = document.querySelector('#show-all-button')
 
 
 // Add onclick for my buttons
+// I found the tool from our Dev mode and other folk's help
+// easy to understnad the logic add and remove
+
 showVideoButton.onclick = () => {
 	// When I click show videos, it should add show-video class
 	channelBlocks.classList.remove('show-video', 'show-image', 'show-text', 'show-link', 'show-audio');
